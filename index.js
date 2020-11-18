fc6d9df4bcc8aa4ada9036eaf4f11b87a5563e14
caiolavos <lavos98@gmail.com>
2020-11-18 04:10:11 -0300

    server

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', function (req, res) {
+   res.send('Hello World from GCE!'); 
 });
 
-const server = app.listen(8080, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log(`Example app listening at http://${host}:${port}`);
+app.listen(3000, function () {
+   console.log('Example app listening on port 3000!');
 });
